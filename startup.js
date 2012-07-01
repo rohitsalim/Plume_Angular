65ec9c428367cacf656262d2218892fd80b722db
Your Name <madhav89@gmail.com>
2012-07-01 18:23:28 -0400

    login page

--- startup.js
+++ startup.js
@@ -4,10 +4,11 @@ var fs = require('fs');
 var url = require('url');
 
 var indexhtml = "";
+var loginhtml = "";
 
 var fileReader = function(path, continuation) {
 	fs.readFile(path, function(err, html) {
-		
+		continuation()
 	});
 }
 
@@ -19,6 +20,14 @@ fs.readFile('./index.html', function (err, html) {
 	}
 });
 
+fs.readFile('./login.html', function (err, html) {
+    if (err) {
+		loginhtml = loginhtml + err;
+    } else {
+		loginhtml = html;
+	}
+});
+
 http.createServer(function (req, res) {
   var url_obj = url.parse(req.url);
   var path = url_obj.pathname;
@@ -26,6 +35,9 @@ http.createServer(function (req, res) {
 	if (path === '/index.html' || path === '/') {
 		res.writeHead(200, {'Content-Type':'text/html'});
 		res.end(indexhtml);
+	} else if (path === '/login.html') {
+		res.writeHead(200, {'Content-Type':'text/html'});
+		res.end(loginhtml);
 	} else {
 		res.writeHead(200, {'Content-Type': 'text/plain'});
 	  	res.end('Hello World\n');		
