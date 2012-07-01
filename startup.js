cff9662cec658fb1f1a8890a2a304ce9b39a0404
Your Name <madhav89@gmail.com>
2012-07-01 18:14:11 -0400

    basic server

--- startup.js
+++ startup.js
@@ -1,8 +1,36 @@
 
 var http=require('http');
+var fs = require('fs');
+var url = require('url');
+
+var indexhtml = "";
+
+var fileReader = function(path, continuation) {
+	fs.readFile(path, function(err, html) {
+		
+	});
+}
+
+fs.readFile('./index.html', function (err, html) {
+    if (err) {
+		indexhtml = indexhtml + err;
+    } else {
+		indexhtml = html;
+	}
+});
+
 http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
+  var url_obj = url.parse(req.url);
+  var path = url_obj.pathname;
+
+	if (path === '/index.html' || path === '/') {
+		res.writeHead(200, {'Content-Type':'text/html'});
+		res.end(indexhtml);
+	} else {
+		res.writeHead(200, {'Content-Type': 'text/plain'});
+	  	res.end('Hello World\n');		
+	}
+
 }).listen(1337, '127.0.0.1');
 console.log('Server running at http://127.0.0.1:1337/');
 
